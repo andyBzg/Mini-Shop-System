f89a72d122426bf110b375b098d0c9c03ab10861
andyBzg <andr.bzg@gmail.com>
2025-05-12 16:31:12 +0200

    Add a repository that is responsible for providing access to the JSON file that is used to store user data

---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,63 @@
+﻿using Application.Interfaces;
+using Application.Models;
+using System.Text.Json;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly string _filePath;
+
+        public UserRepository(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        private Dictionary<Guid, User> LoadUsers()
+        {
+            if (!File.Exists(_filePath))
+                return new Dictionary<Guid, User>();
+
+            string jsonString = File.ReadAllText(_filePath);
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+            };
+
+            Dictionary<Guid, User>? users = JsonSerializer.Deserialize<Dictionary<Guid, User>>(jsonString, options);
+
+            return users ?? new Dictionary<Guid, User>();
+        }
+
+        private void SaveUsers(Dictionary<Guid, User> users)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+            };
+
+            string jsonString = JsonSerializer.Serialize(users, options);
+
+            File.WriteAllText(_filePath, jsonString);
+        }
+
+        public void Add(Guid guid, User user)
+        {
+            Dictionary<Guid, User> users = LoadUsers();
+
+            if (!users.ContainsKey(guid))
+            {
+                users.Add(guid, user);
+                SaveUsers(users);
+            }
+        }
+
+        public User? GetUserByEmail(string email)
+        {
+            Dictionary<Guid, User> users = LoadUsers();
+            return users.Values.FirstOrDefault(u => u.Email == email);
+        }
+    }
+}
