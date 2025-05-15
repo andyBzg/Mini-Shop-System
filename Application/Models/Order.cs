5d41e293cdb7a2655cd8bf14235fa68211182ed4
andyBzg <andr.bzg@gmail.com>
2025-05-15 16:48:51 +0200

    Update Order class to include List<CartItem> and IsConfirmed flag

--- Application/Models/Order.cs
+++ Application/Models/Order.cs
@@ -1,11 +1,12 @@
 ﻿namespace Application.Models
 {
-    internal class Order
+    public class Order
     {
         public Guid Id { get; init; } = Guid.NewGuid();
         public Guid UserId { get; set; }
-        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public List<CartItem> Items { get; set; } = new List<CartItem>();
         public decimal TotalPrice { get => Items.Sum(item => item.TotalPrice); }
         public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
+        public bool IsConfirmed { get; set; }
     }
 }
