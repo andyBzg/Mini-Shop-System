453e32b808e74766dbcd9dd88a33c841ddecbb6a
andyBzg <andr.bzg@gmail.com>
2025-05-15 16:49:09 +0200

    Create OrderMenu class (empty for now)

---
+++ Presentation/Views/OrderMenu.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+
+namespace Presentation.Views
+{
+    internal class OrderMenu
+    {
+        private readonly IOrderService _orderService;
+        private readonly IProductService _productService;
+        private readonly Guid _userId;
+
+        public OrderMenu(IOrderService orderService, IProductService productService, Guid userId)
+        {
+            _orderService = orderService;
+            _productService = productService;
+            _userId = userId;
+        }
+
+        public void RunOrderMenu()
+        {
+            //TODO implement order menu
+        }
+    }
+}
