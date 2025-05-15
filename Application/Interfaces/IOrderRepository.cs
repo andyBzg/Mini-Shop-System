ab347edea552d921526bc7a5fba4c67876ab85e5
andyBzg <andr.bzg@gmail.com>
2025-05-15 16:47:59 +0200

    Add IOrderRepository and stub implementation with SaveOrder and LoadOrders

---
+++ Application/Interfaces/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Models;
+
+namespace Application.Interfaces
+{
+    public interface IOrderRepository
+    {
+        void SaveOrder(Order order);
+        List<Order> LoadOrders();
+    }
+}
---
+++ Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Application.Interfaces;
+using Application.Models;
+
+namespace Infrastructure.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly string _filePath;
+
+        public OrderRepository(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<Order> LoadOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
